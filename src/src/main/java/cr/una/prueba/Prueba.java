24f8e27696949e805516a99b0b1602a624ba5c0c
Rodlar13 <alexa1398@gmail.com>
2020-04-16 10:00:35 -0600

    Create a package and class "prueba"

---
+++ src/src/main/java/cr/una/prueba/Prueba.java
@@ -0,0 +1,19 @@
+package cr.una.prueba;
+
+public class Prueba {
+    public String nom;
+
+    public Prueba(String nom) {
+        this.nom = nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+
+}
